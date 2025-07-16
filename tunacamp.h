cc218431dab40d3fe17b9906bc5d7f0f9111f546
Jairo Rick <kip.jairorick@gmail.com>
2025-07-16 09:28:36 +0300

    A lot of refactoring

--- tunacamp.h
+++ tunacamp.h
@@ -14,94 +14,55 @@
 #define DOCID_SIZE 13
 
 
-enum recordType {documents, sessions, projects};
-enum docType {book, documentation, article, periodical};
-enum docPriority {core, essential, important, misc};
+enum bookPriority {core, essential, important, misc};
 enum status {complete, incomplete};
-enum sessionType {study, programming, art};
+enum sessionType {study, project};
 
 
-typedef struct {
+struct session {
 	int type;
 	time_t startTime;
 	time_t stopTime;
 	char* id;
 	int startPage;
 	int stopPage;
-} session;
+};
 
 
-typedef struct {
-	int type;
+struct book {
 	char* id;
 	char* title;
-	int startPage;	// The absolute pages
-	int stopPage;	// in evince including only the core info.
+	int minPage;	// The absolute pages
+	int maxPage;	// in evince including only the core info.
 	int priority;
-	int status;
-} document;
+};
 
 
-typedef struct {
-	enum status status;
+struct project {
 	char* name;
 	char* description;
-} project;
-
-
-extern session** sessionList;
-extern document** documentList;
-extern project** projectList;
-
-extern int sessionCount;
-extern int documentCount;
-extern int projectCount;
-
-extern FILE* documentListFile;
-extern FILE* sessionListFile;
-extern FILE* projectListFile;
+};
 
 
 
-/*********************************************
- * module parser.c:                          *
- * Functions for reading and writing records *
- * *******************************************/
-
-/* struct record** parserecords(FILE*/
 int parseList(FILE*, const int);
 void freeList(const int);
-int writeList(const int);
+struct session** parseSessions(FILE*, int*);
+struct project** parseProjecs(FILE*, int*);
+int writeSessions(FILE*, struct session**, const int);
 
 
-/********************************
- * module progress.c            *
- * ******************************/
-void printActivity(void);
+void printActivity(struct session**, const int, struct book**, const int);
 void printToday(void);
 void printSummary(void);
 
 
-/*******************************
- * module library.c              *
- * *****************************/
 char* genDocId(void);
-void printLibrary(void);
+void printBooks(struct book**, const int);
 int updateLibrary(void);
 
 
-/*************************
- * module sessions.c
- * ***********************/
-int startSession(void);
-int pauseSession(void);
-void resumeSession(void);
-void stopSession(void);
-
-/************************
- * module projects.c
- * *********************/
-void printProjects(void);
+void printProjects(struct project**, const int);
 int addProject(void);
 int deleteProject(void);
 int editProjectInfo(void);
